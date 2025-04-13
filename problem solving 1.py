d8e8194766e2808d17c75f8cc240548122464ef6
zamzamsaid <zamzamalsalti29@gmail.com>
2025-04-13 19:32:48 +0400

    Add files via upload

---
+++ problem solving 1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+candle_input=[4,1,4,3]
+tallest_candles= max(candle_input)
+num_candles= candle_input.count(tallest_candles)
+print(num_candles)
+
+
+# In[ ]:
+
+
+
+
